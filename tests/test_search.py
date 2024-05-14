ae855ad4d4c18f9b164b780960840ba4c4721ac3
drizzy2004 <157053650+drizzy2004@users.noreply.github.com>
2024-05-14 11:46:17 -0600

    Finished implementing chapter 4

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,22 +2,32 @@
 These tests cover DuckDuckGo searches
 """
 
+from pages.search import DuckDuckGoSearchPage
+from pages.result import DuckDuckGoResultPage
+
 
 def test_basic_duckduckgo_search(browser):
+    # Here we use the browser we get from the fixture to construct the two different pages 
+    search_page = DuckDuckGoSearchPage(browser)
+    result_page = DuckDuckGoResultPage(browser)
+    PHRASE = "Panda"
     
     # Given the duckduckgo homepage is displayed
-    # TODO 
+    search_page.load()
 
     # When the user searched for "panda"
-    # TODO
+    search_page.search(PHRASE)
 
     # Then the seach result title contains "panda"
-    # TODO 
+    assert PHRASE in result_page.title()
 
     # And the search result query is "panda"
-    # TODO
+    assert PHRASE == result_page.search_input_value()
 
     # And the search result links pertain to "panda"
-    # TODO 
+    titles = result_page.result_link_titles()
+    matches = [t for t in titles if PHRASE.lower() in t.lower()]
+    assert len(matches) > 0
 
+    # TODO remove this exception once the test case is complete
     raise Exception("Incomplete Test")
